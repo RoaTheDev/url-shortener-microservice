fba227bfe09e14882d0ee1dc7e56816b0d4f4bef
RoaTheDev <roachamreun15@proton.me>
2025-09-08 22:59:02 +0700

    inital setup
    this is the intial setup for the URL shortener Microservice using .NET,
    Cassandra, SQL Server, Redis, and other essential tools.

---
+++ SharedContracts/UrlEvents.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedContracts;
+
+public record UrlCreatedEvent(
+    Guid UrlId,
+    string ShortCode,
+    string OriginalUrl,
+    Guid UserId,
+    DateTime CreatedAt
+);
+
+public record UrlAccessedEvent(
+    Guid UrlId,
+    string ShortCode,
+    string UserAgent,
+    string IpAddress,
+    DateTime AccessedAt);
+
+public record UrlDeletedEvent(
+    Guid UrlId,
+    string ShortCode,
+    Guid UserId,
+    DateTime DeletedAt);
